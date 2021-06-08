860273a125591db7ee92852cec38a17f23d3f305
Paolo <paolo.scanferlato@gmail.com>
2021-06-08 17:02:01 +0200

    Aggiunti su MapRepositoryRedisTest i test getCell, getCells e setCells

--- src/test/java/org/linecode/server/persistence/MapRepositoryRedisTest.java
+++ src/test/java/org/linecode/server/persistence/MapRepositoryRedisTest.java
@@ -24,43 +24,60 @@ public class MapRepositoryRedisTest {
     }
 
     @Test
-    public void getLength() {
+    public void TestGetLength() {
         when(db.get("length")).thenReturn("5");
         assertEquals(5, test.getLength());
         verify(db,times(1)).get("length");
     }
 
     @Test
-    public void getHeight() {
+    public void TestGetHeight() {
         when(db.get("height")).thenReturn("5");
         assertEquals(5, test.getHeight());
         verify(db,times(1)).get("height");
     }
 
+    @Test
+    public void TestGetCell() {
+        when(db.hget("cell:0:0","locked")).thenReturn("false");
+        when(db.hget("cell:0:0","base")).thenReturn("false");
+        when(db.hget("cell:0:0","direction")).thenReturn("RIGHT");
+        when(db.hget("cell:0:0","poi")).thenReturn("false");
+        Cell cell= test.getCell(0,0);
+        assertFalse(cell.isLocked());
+        assertFalse(cell.isBase());
+        assertFalse(cell.isPoi());
+        assertEquals(Direction.RIGHT,cell.getDirection());
+        verify(db,times(4)).hget(anyString(),anyString());
+    }
 
-    // TODO: rifare test
-//    @Test
-//    public void getCell() {
-//        when(db.hget("cell:0:0","locked")).thenReturn("false");
-//        when(db.hget("cell:0:0","base")).thenReturn("false");
-//        when(db.hget("cell:0:0","direction")).thenReturn("RIGHT");
-//        Cell cell= test.getCell(0,0);
-//        assertFalse(cell.isLocked());
-//        assertFalse(cell.isBase());
-//        assertEquals(Direction.RIGHT,cell.getDirection());
-//    }
+    @Test
+    public void TestSetCells() {
+        when(db.get("length")).thenReturn("5");
+        when(db.get("height")).thenReturn("5");
+        List<Cell> cellList = new ArrayList<Cell>();
+        cellList.add(new Cell(new Position(0,0), false, false, Direction.ALL, false));
+        cellList.add(new Cell(new Position(0,1), false, false, Direction.ALL, false));
+        cellList.add(new Cell(new Position(1,0), false, false, Direction.ALL, false));
+        cellList.add(new Cell(new Position(1,1), false, false, Direction.ALL, false));
+        test.setCells(cellList, 2, 2);
+        verify(db,times((5+1)*(5+1))).get(anyString());
+        verify(db,times(5*5)).del(anyString());
+        verify(db,times(2)).set(anyString(),anyString());
+        verify(db,times(2*2)).hmset(anyString(),anyMap());
+        verify(db,times(1)).bgsave();
+    }
 
-    // TODO: rifare test
-//    @Test
-//    public void setCells() {
-//        List<Cell> cellList = new ArrayList<Cell>();
-//        cellList.add(new Cell(new Position(0,0), false, false, Direction.RIGHT, true));
-//        cellList.add(new Cell(new Position(0,1), true, false, Direction.RIGHT, false));
-//        cellList.add(new Cell(new Position(1,0), false, true, Direction.ALL, false));
-//        cellList.add(new Cell(new Position(1,1), false, true, Direction.ALL, false));
-//        test.setCells(cellList, 2, 2);
-//        verify(db, times(1)).del(anyString());
-//        verify(db, times(3)).hmset(anyString(), anyMap());
-//        verify(db, times(1)).bgsave();
-//    }
+    @Test
+    public void TestGetCells() {
+        when(db.get("length")).thenReturn("5");
+        when(db.get("height")).thenReturn("5");
+        when(db.hget(anyString(),eq("locked"))).thenReturn("false");
+        when(db.hget(anyString(),eq("base"))).thenReturn("false");
+        when(db.hget(anyString(),eq("direction"))).thenReturn("RIGHT");
+        when(db.hget(anyString(),eq("poi"))).thenReturn("false");
+        List<Cell> cellList = test.getCells();
+        verify(db,times((5+1)*(5+1))).get(anyString());
+        verify(db,times(5*5*4)).hget(anyString(),anyString());
+    }
 }
